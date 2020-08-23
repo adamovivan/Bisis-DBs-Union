c6df94f721f94a43551a4436196211df7fcd2b55
adamovivan <adamovivan@gmail.com>
2020-08-23 13:13:22 +0200

    Union database name changed

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
   public static final String BMB = "bmb";
   public static final String MSK = "msk";
 
-  public static final String UNION_RECORDS = "a_test_1_union_records";
+  public static final String UNION_RECORDS = "union_records";
   public static final String BGB_RECORDS = "bgb_records";
   public static final String GBNS_RECORDS = "gbns_records";
   public static final String BS_RECORDS = "bs_records";
